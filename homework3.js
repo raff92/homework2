811a8ba9376bc144f26ff5d9f78491331ec9292a
raff92 <macho.vaganyan75@mail.ru>
2024-10-01 19:30:51 +0300

    Add files via upload

---
+++ homework3.js
@@ -0,0 +1,68 @@
+// 1 задание
+
+n = 3478
+
+
+
+3478
+n = (n - (n % 10)) 
+
+
+3470
+n = (n - (n % 10)) / 10
+347
+for (let n = 3478; n > 0; n = (n - (n % 10)) / 10 ) {
+    console.log ( n % 10 )
+} 
+
+// 
+
+n = 50
+
+n = (n - (n % 5)) 
+
+n = (n - (n % 5)) / 5
+
+for (let n = 50; n > 0; n = (n - (n % 5)) / 5 ) {
+    console.log ( n % 5 )
+}
+
+
+// 2 Задание 
+
+let a = 1 * 2 * 3 * 4
+
+//
+
+let a = 4 * 5 * 0 
+
+// 3 Задание
+
+
+let new1 = 1 * 2 * 3 * 3
+let new2 = 1 + 2 + 3 + 3
+
+new1 / new2
+
+//
+
+let new1 = 5
+let new2 = 5 
+
+new1 / new2
+
+// 
+let new1 = 0
+let new2 = 0
+
+new1 / new2
+
+// 
+
+let new1 = 4 * 5 * 5
+let new2 = 4 + 5 + 5
+
+new1 / new2 // получается 7.14.....
+
+new1 % new2 // 2 
+
